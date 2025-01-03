0764cf537762b1ae21ef56aa14bbad8c6ef9cb37
AdrianDominguezCal <adriandominguez@usal.es>
2025-01-03 17:37:13 +0100

    Add files via upload

--- codigo/config/db_proyecto_D.php
+++ codigo/config/db_proyecto_D.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=daw2_2024_D_xxxxx',
+    'dsn' => 'mysql:host=localhost;dbname=daw_proyectod',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
