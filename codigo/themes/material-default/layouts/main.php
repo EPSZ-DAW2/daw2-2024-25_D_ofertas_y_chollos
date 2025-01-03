29fa7826beaccfba57c4cac59ae456c22649e1cf
alexvilaa <id00802395@usal.es>
2025-01-03 12:16:11 +0100

    Update main.php

--- codigo/themes/material-default/layouts/main.php
+++ codigo/themes/material-default/layouts/main.php
@@ -8,56 +8,57 @@ use yii\widgets\Breadcrumbs;
  * @var $this \yii\base\View
  * @var $content string
  */
-// $this->registerAssetBundle('app');
+
 ?>
 <?php $this->beginPage(); ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
   
-  <title><?php echo Html::encode($this->title); ?></title>
+  <title><?= Html::encode($this->title ?: 'Ofertas y Chollos') ?></title>
   <?php $this->head(); ?>
-	
+  
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
   
-  <!-- CSS  -->
-  <link href="<?php echo $this->theme->baseUrl ?>/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-  <link href="<?php echo $this->theme->baseUrl ?>/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <!-- CSS -->
+  <link href="<?= $this->theme->baseUrl ?>/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="<?= $this->theme->baseUrl ?>/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 <body>
-  <?php $this->beginBody() ?>
+  <?php $this->beginBody(); ?>
+
   <nav class="light-blue lighten-1" role="navigation">
     <div class="container">
-      <div class="nav-wrapper"><a id="logo-container" href="#" class="brand-logo"><?php echo Html::encode(\Yii::$app->name); ?></a>
-	  		<?php
-						echo Menu::widget([
-						    'options' => ['id' => "nav-mobile", 'class' => 'right side-nav'],
-						    'items' => [
-						        ['label' => 'Home', 'url' => ['site/index']],
-						        ['label' => 'About', 'url' => ['site/about']],
-						        ['label' => 'Contact', 'url' => ['site/contact']],
-						        ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
-						    ],
-						]);
-					?>
-          <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
+      <div class="nav-wrapper">
+        <a href="<?= Yii::$app->homeUrl ?>" id="logo-container" class="brand-logo"><?= Html::encode(Yii::$app->name) ?></a>
+        <?= Menu::widget([
+            'options' => ['id' => 'nav-mobile', 'class' => 'right side-nav'],
+            'items' => [
+                ['label' => 'Inicio', 'url' => ['site/index']],
+                ['label' => 'Acerca de', 'url' => ['site/about']],
+                ['label' => 'Contacto', 'url' => ['site/contact']],
+                ['label' => 'Iniciar sesión', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
+            ],
+        ]); ?>
+        <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
       </div>
     </div>
   </nav>
+
   <div class="section no-pad-bot" id="index-banner">
     <div class="container">
       <br><br>
-      <h1 class="header center orange-text"><?php echo Html::encode(\Yii::$app->name); ?></h1>
+      <h1 class="header center orange-text"><?= Html::encode(Yii::$app->name) ?></h1>
       <div class="row center">
-        <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+        <h5 class="header col s12 light">Una plataforma moderna y responsive basada en Material Design</h5>
       </div>
       <div class="row center">
-        <a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light orange">Get Started</a>
+        <a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light orange">Comenzar</a>
       </div>
       <br><br>
-
     </div>
   </div>
 
@@ -65,7 +66,7 @@ use yii\widgets\Breadcrumbs;
     <div class="section">
       <div class="row">
         <div class="col s12 m12">
-          <?php echo $content; ?>
+          <?= $content; ?>
         </div>
       </div>
     </div>
@@ -73,88 +74,81 @@ use yii\widgets\Breadcrumbs;
 
   <div class="container">
     <div class="section">
-
-      <!--   Icon Section   -->
+      <!-- Icon Section -->
       <div class="row">
         <div class="col s12 m4">
           <div class="icon-block">
-            <h2 class="center light-blue-text"><i class="mdi-image-flash-on"></i></h2> 
-            <h5 class="center">Speeds up development</h5>
-
-            <p class="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
+            <h2 class="center light-blue-text"><i class="material-icons">flash_on</i></h2> 
+            <h5 class="center">Acelera el desarrollo</h5>
+            <p class="light">Hemos creado un diseño predeterminado que incorpora nuestros componentes personalizados, con animaciones y transiciones optimizadas para una experiencia fluida.</p>
           </div>
         </div>
-
         <div class="col s12 m4">
           <div class="icon-block">
-            <h2 class="center light-blue-text"><i class="mdi-social-group"></i></h2>
-            <h5 class="center">User Experience Focused</h5>
-
-            <p class="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
+            <h2 class="center light-blue-text"><i class="material-icons">group</i></h2>
+            <h5 class="center">Enfocado en la experiencia del usuario</h5>
+            <p class="light">Al usar principios de Material Design, creamos un sistema responsive unificado para una mejor experiencia en todas las plataformas.</p>
           </div>
         </div>
-
         <div class="col s12 m4">
           <div class="icon-block">
-            <h2 class="center light-blue-text"><i class="mdi-action-settings"></i></h2>
-            <h5 class="center">Easy to work with</h5>
-
-            <p class="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
+            <h2 class="center light-blue-text"><i class="material-icons">settings</i></h2>
+            <h5 class="center">Fácil de usar</h5>
+            <p class="light">Ofrecemos documentación detallada y ejemplos de código para que los nuevos usuarios puedan comenzar rápidamente.</p>
           </div>
         </div>
       </div>
-      
     </div>
     <br><br>
-
-    <div class="section">
-
-    </div>
   </div>
 
   <footer class="orange">
     <div class="container">
       <div class="row">
         <div class="col l6 s12">
-          <h5 class="white-text">Company Bio</h5>
-          <p class="grey-text lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
-
-
+          <h5 class="white-text">Sobre Nosotros</h5>
+          <p class="grey-text lighten-4">Somos un equipo de estudiantes trabajando en este proyecto como si fuera nuestro trabajo a tiempo completo. Tu apoyo es muy apreciado.</p>
         </div>
         <div class="col l3 s12">
-          <h5 class="white-text">Settings</h5>
+          <h5 class="white-text">Enlaces</h5>
           <ul>
-            <li><a class="white-text" href="#!">Link 1</a></li>
-            <li><a class="white-text" href="#!">Link 2</a></li>
-            <li><a class="white-text" href="#!">Link 3</a></li>
-            <li><a class="white-text" href="#!">Link 4</a></li>
+            <li><a class="white-text" href="#!">Enlace 1</a></li>
+            <li><a class="white-text" href="#!">Enlace 2</a></li>
+            <li><a class="white-text" href="#!">Enlace 3</a></li>
+            <li><a class="white-text" href="#!">Enlace 4</a></li>
           </ul>
         </div>
         <div class="col l3 s12">
-          <h5 class="white-text">Connect</h5>
+          <h5 class="white-text">Conecta</h5>
           <ul>
-            <li><a class="white-text" href="#!">Link 1</a></li>
-            <li><a class="white-text" href="#!">Link 2</a></li>
-            <li><a class="white-text" href="#!">Link 3</a></li>
-            <li><a class="white-text" href="#!">Link 4</a></li>
+            <li><a class="white-text" href="#!">Facebook</a></li>
+            <li><a class="white-text" href="#!">Twitter</a></li>
+            <li><a class="white-text" href="#!">LinkedIn</a></li>
+            <li><a class="white-text" href="#!">Instagram</a></li>
           </ul>
         </div>
       </div>
     </div>
     <div class="footer-copyright">
       <div class="container">
-      Made by <a class="orange-text lighten-3" href="http://materializecss.com">Materialize</a>
+        © <?= date('Y') ?> Ofertas y Chollos. Todos los derechos reservados.
+        <a class="orange-text lighten-3" href="#">Política de Privacidad</a>
       </div>
     </div>
-  </footer>  
+  </footer>
 
-
-  <!--  Scripts-->
+  <!-- Scripts -->
   <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-  <script src="<?php echo $this->theme->baseUrl ?>/js/materialize.js"></script>
-  <script src="<?php echo $this->theme->baseUrl ?>/js/init.js"></script>
+  <script src="<?= $this->theme->baseUrl ?>/js/materialize.js"></script>
+  <script src="<?= $this->theme->baseUrl ?>/js/init.js"></script>
+  <script>
+    document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('.sidenav');
+      M.Sidenav.init(elems);
+    });
+  </script>
   
-  <?php $this->endBody() ?>
-  </body>
+  <?php $this->endBody(); ?>
+</body>
 </html>
 <?php $this->endPage(); ?>
