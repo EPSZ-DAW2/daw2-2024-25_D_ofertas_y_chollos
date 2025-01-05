92d3b121251c67c0d423c482c65cf3ab7faba487
AleAloVic <alejandroalovic@usal.es>
2025-01-05 19:12:14 +0100

    Controlador de categorias y primera vista

---
+++ codigo/controllers/CategoriasController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use app\models\Categorias;
+use yii\data\ActiveDataProvider;
+
+class CategoriasController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+        $this->view->title = 'Ofertas y Chollos - Categorías';
+
+        // Mostramos todas las categorías
+        $query = PartidosJornada::find();
+        $categorias = $query->all();
+
+        return $this->render('index', [
+            'categorias'->$categorias;
+        ]);
+    }
+}
+?>
---
+++ codigo/views/categorias/index.php
@@ -0,0 +1,17 @@
+<?php 
+use yii\helpers\Html;
+use app\models\Categorias;
+?>
+
+<div class='site-index'>
+    <h1>CATEGORÍAS</h1> 
+
+    <div class="body-content">
+        <?php foreach ($categorias as $categoria): ?>
+            <h2><?= Html::a($categoria->nombre)?></h2>
+            <p>Descripción: <?= Html::a($categoria->descripcion) ?></p>
+            <?php if($categoria->categoria_padre_id != NULL): ?>
+                <?= Html::a($categoria->categoria_padre_id)?>
+        <?php endforeach; ?>
+    </div>
+</div>
