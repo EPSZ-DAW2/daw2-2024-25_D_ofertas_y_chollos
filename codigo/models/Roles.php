8021a0ca1b637ecbbc7c3e77cb8df89e7b5d54ce
javimanso <jamancon5@gmail.com>
2025-01-07 17:27:43 +0100

    Generar modelo Roles

---
+++ codigo/models/Roles.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "roles".
+ *
+ * @property int $id
+ * @property string $nombre
+ *
+ * @property Usuario[] $usuarios
+ */
+class Roles extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'roles';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['nombre'], 'required'],
+            [['nombre'], 'string', 'max' => 50],
+            [['nombre'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'nombre' => Yii::t('app', 'Nombre'),
+        ];
+    }
+
+    /**
+     * Gets query for [[Usuarios]].
+     *
+     * @return \yii\db\ActiveQuery|yii\db\ActiveQuery
+     */
+    public function getUsuarios()
+    {
+        return $this->hasMany(Usuario::class, ['rol' => 'id']);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return RolesQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new RolesQuery(get_called_class());
+    }
+}
