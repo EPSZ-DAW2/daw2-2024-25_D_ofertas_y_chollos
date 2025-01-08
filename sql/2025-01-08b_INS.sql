eacf080c3878b41b388319bf722a4aeb145941f1
aaronmc26 <150802257+aaronmc26@users.noreply.github.com>
2025-01-08 19:02:20 +0100

    Se añaden datos

---
+++ sql/2025-01-08b_INS.sql
@@ -0,0 +1,16 @@
+USE `daw_proyectod`;
+
+-- INSERCIÓN DE DATOS INICIALES PARA LA TABLA `anuncio`
+
+
+
+
+INSERT INTO `anuncios` (`titulo`, `descripcion`, `precio`, `fecha`, `oferta_id`)
+SELECT 
+    CONCAT('Anuncio ', `titulo`),
+    CONCAT('Descripcion del anuncio de la ', `titulo`),
+    `precio_actual`,
+    `fecha_fin`,
+    `id`
+FROM `ofertas`
+WHERE `anuncio_destacado` = true;
