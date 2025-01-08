2db13f0886c71e806170f6259e3858158b40fb43
javimanso <jamancon5@gmail.com>
2025-01-08 01:06:20 +0100

    Mensajes de exito en ficha Admin

--- codigo/views/usuarios/fichaUsuariosAdmin.php
+++ codigo/views/usuarios/fichaUsuariosAdmin.php
@@ -15,6 +15,16 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h1><?= Html::encode($this->title) ?></h1>
 
+    <?php if (Yii::$app->session->hasFlash('success')): ?>
+        <div class="alert alert-success">
+            <?= Yii::$app->session->getFlash('success') ?>
+        </div>
+    <?php elseif (Yii::$app->session->hasFlash('error')): ?>
+        <div class="alert alert-danger">
+            <?= Yii::$app->session->getFlash('error') ?>
+        </div>
+    <?php endif; ?>
+
 
     <p>
         <?= Html::a(Yii::t('app', 'Crear nuevo usuarios'), ['create']) ?>
