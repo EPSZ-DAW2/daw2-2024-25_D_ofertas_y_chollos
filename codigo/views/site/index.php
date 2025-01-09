76c9f1feca2eae7e9c8f17d014d76cd3a9273806
alexvilaa <id00802395@usal.es>
2025-01-09 17:09:36 +0100

    Update index.php

--- codigo/views/site/index.php
+++ codigo/views/site/index.php
@@ -6,63 +6,50 @@ $this->title = 'Ofertas y Chollos';
 ?>
 <div class="site-index">
 
-    
-        <!-- CONTENEDOR LADO IZQUIERDO -->
-        <div class = "contenedor-categorias">
-            
-            <aside class ="categorias">
-                <p><a style="color: #000;" href="<?= Yii::$app->urlManager->createUrl(['categorias/index']) ?>"><strong>Categorías</strong></a></p>
-                <ul>
-                    <li><a href="#">Tecnología</a></li>
-                    <li><a href="#">Hogar</a></li>
-                    <li><a href="#">Moda</a></li>
-                    <li><a href="#">Deportes</a></li>
-                    <li><a href="#">Juguetes</a></li>
-                    <li><a href="#">Automóvil</a></li>
-                    <li><a href="#">Más...</a></li>
-                </ul>
-            </aside>
+    <!-- CONTENEDOR ARRIBA (CATEGORÍAS) -->
+    <div class="contenedor-categorias">
+        <aside class="categorias">
+            <p><a style="color: #000;" href="<?= Yii::$app->urlManager->createUrl(['categorias/index']) ?>"><strong>Categorías</strong></a></p>
+            <ul>
+                <li><a href="#">Tecnología</a></li>
+                <li><a href="#">Hogar</a></li>
+                <li><a href="#">Moda</a></li>
+                <li><a href="#">Deportes</a></li>
+                <li><a href="#">Juguetes</a></li>
+                <li><a href="#">Automóvil</a></li>
+                <li><a href="#">Más...</a></li>
+            </ul>
+        </aside>
+    </div>
 
-        </div>
-        
-
-        <!-- CONTENEDOR LADO DERECHO -->
-        <div id = "contenedor-con-ofertas">
-
-            <div class="jumbotron text-center bg-transparent">
-                
-                <h1 class="texto-inicial">Ofertas exclusivas que no querrás perderte</h1>
-                <p style ="font-size: 20px; font-weight: bold;">Aprovecha los mejores precios del mercado</p>
+    <!-- CONTENEDOR DEBAJO (OFERTAS) -->
+    <div class="jumbotron">
+        <h1 class="texto-inicial">Ofertas exclusivas que no querrás perderte</h1>
+        <p style="font-size: 20px; font-weight: bold;">Aprovecha los mejores precios del mercado</p>
+        <div class="offers">
+            <div class="offer">
+                <h3>Oferta 1</h3>
+                <p>Descripción breve de la oferta 1.</p>
+                <button>Ver más</button>
             </div>
-    
-            <!--
-            <div class="container">
-                <div class="section">
-                  <div class="row">
-                    <div class="col s12 m4">
-                      <div class="icon-block">
-                        <h2 class="center light-blue-text"><i class="material-icons">flash_on</i></h2> 
-                        <h5 class="center">Acelera el desarrollo</h5>
-                        <p class="light">Hemos creado un diseño predeterminado que incorpora nuestros componentes personalizados, con animaciones y transiciones mizadas para una experiencia fluida.</p>
-                      </div>
-                    </div>
-                    <div class="col s12 m4">
-                      <div class="icon-block">
-                        <h2 class="center light-blue-text"><i class="material-icons">group</i></h2>
-                        <h5 class="center">Enfocado en la experiencia del usuario</h5>
-                        <p class="light">Al usar principios de Material Design, creamos un sistema responsive unificado para una mejor experiencia en todas las aformas.</p >
-                      </div>
-                    </div>
-                    <div class="col s12 m4">
-                      <div class="icon-block">
-                        <h2 class="center light-blue-text"><i class="material-icons">settings</i></h2>
-                        <h5 class="center">Fácil de usar</h5>
-                        <p class="light">Ofrecemos documentación detallada y ejemplos de código para que los nuevos usuarios puedan comenzar rápidamente.</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <br><br>
-            </div>-->
-        </div>       
+            <div class="offer">
+                <h3>Oferta 2</h3>
+                <p>Descripción breve de la oferta 2.</p>
+                <button>Ver más</button>
+            </div>
+            <div class="offer">
+                <h3>Oferta 3</h3>
+                <p>Descripción breve de la oferta 3.</p>
+                <button>Ver más</button>
+            </div>
+            <div class="offer">
+                <h3>Oferta 4</h3>
+                <p>Descripción breve de la oferta 4.</p>
+                <button>Ver más</button>
+            </div>
+           
+            </div>
+        </div>
+    </div>
+
 </div>
