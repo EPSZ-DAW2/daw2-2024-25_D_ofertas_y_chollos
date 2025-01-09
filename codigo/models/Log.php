249305160351b91005aee35089d0dd5af97f651c
aaronmc26 <150802257+aaronmc26@users.noreply.github.com>
2025-01-09 18:42:01 +0100

    para los logs

---
+++ codigo/models/Log.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "logs".
+ *
+ * @property int $id
+ * @property string|null $fecha_hora
+ * @property string|null $nivel
+ * @property string|null $modulo
+ * @property string|null $descripcion
+ */
+class Log extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'logs';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['fecha_hora'], 'safe'],
+            [['nivel'], 'string'],
+            [['modulo'], 'string', 'max' => 50],
+            [['descripcion'], 'string', 'max' => 250],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'fecha_hora' => Yii::t('app', 'Fecha Hora'),
+            'nivel' => Yii::t('app', 'Nivel'),
+            'modulo' => Yii::t('app', 'Modulo'),
+            'descripcion' => Yii::t('app', 'Descripcion'),
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return LogsQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new LogsQuery(get_called_class());
+    }
+}
