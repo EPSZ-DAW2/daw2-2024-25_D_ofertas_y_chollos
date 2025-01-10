2d4732a5eda4631aec2ab94ee876787c172eb293
Álvaro Arango <git@alvaroarango.com>
2025-01-10 14:32:43 +0100

    Configuraciones: controlador de limpieza

---
+++ codigo/controllers/LimpiezaController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace app\console\controllers;
+
+use Yii;
+use yii\console\Controller;
+
+class LimpiezaController extends Controller
+{
+    /**
+     * Elimina registros obsoletos de usuarios, comentarios, etc.
+     */
+    public function actionRun()
+    {
+        $diasObsoletos = Yii::$app->params['diasRegistroObsoleto'];
+        $fechaLimite = date('Y-m-d H:i:s', strtotime("-{$diasObsoletos} days"));
+
+        // Eliminar usuarios obsoletos
+        Usuarios::deleteAll(['<', 'fecha_registro', $fechaLimite, 'registro_confirmado' => 0]);
+
+        // Eliminar comentarios obsoletos
+        Comentario::deleteAll(['<', 'fecha_creacion', $fechaLimite]);
+
+        echo "Eliminación de registros obsoletos completada.\n";
+    }
+}
