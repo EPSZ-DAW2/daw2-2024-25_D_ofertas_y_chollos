892c4fb8a7de408010f82f6b91154793bad79ebd
alexvilaa <id00802395@usal.es>
2025-01-10 01:53:34 +0100

    Update index.php

--- codigo/views/site/index.php
+++ codigo/views/site/index.php
@@ -65,6 +65,36 @@ $this->title = 'Ofertas y Chollos';
     </div>
 
 
+    <!--
+  <div class="container">
+    <div class="section">
+      
+      <div class="row">
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center light-blue-text"><i class="material-icons">flash_on</i></h2> 
+            <h5 class="center">Acelera el desarrollo</h5>
+            <p class="light">Hemos creado un diseño predeterminado que incorpora nuestros componentes personalizados, con animaciones y transiciones optimizadas para una experiencia fluida.</p>
+          </div>
+        </div>
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center light-blue-text"><i class="material-icons">group</i></h2>
+            <h5 class="center">Enfocado en la experiencia del usuario</h5>
+            <p class="light">Al usar principios de Material Design, creamos un sistema responsive unificado para una mejor experiencia en todas las plataformas.</p>
+          </div>
+        </div>
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center light-blue-text"><i class="material-icons">settings</i></h2>
+            <h5 class="center">Fácil de usar</h5>
+            <p class="light">Ofrecemos documentación detallada y ejemplos de código para que los nuevos usuarios puedan comenzar rápidamente.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <br><br>
+  </div>-->
 
 
 </div>
