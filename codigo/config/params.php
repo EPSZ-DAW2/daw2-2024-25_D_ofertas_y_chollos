f65ae0dbf160455d417cf7555b44a14169825e7a
Álvaro Arango <git@alvaroarango.com>
2025-01-17 23:09:54 +0100

    Configuraciones: límite de intentos de acceso fallidos actualizado

--- codigo/config/params.php
+++ codigo/config/params.php
@@ -7,7 +7,7 @@ return [
     // Mantenimiento de configuraciones
     'limiteComentarios' => 100, // Límite de comentarios por usuario
     'limiteValoraciones' => 50, // Límite de valoraciones por usuario
-    'intentosAccesoFallidos' => 5, // Intentos de acceso fallidos permitidos
+    'intentosAccesoFallidos' => 3, // Intentos de acceso fallidos permitidos
     'rutaBackup' => '@app/backups', // Ruta para almacenar las copias de seguridad
     'diasRegistroObsoleto' => 30, // Días para considerar un registro como obsoleto
     'palabrasProhibidas' => ['palabra1', 'palabra2', 'palabra3'], // Lista de palabras prohibidas
