32d2c449bd0966c2a12bc27483a07ad02885ef6d
javimanso <jamancon5@gmail.com>
2025-01-18 16:49:55 +0100

    Mantenimiento de comentarios de usuarios
    Crear el controlador y las vistas, modificar modelos en Comentario

---
+++ codigo/views/usuario-comentario/index.php
@@ -0,0 +1,62 @@
+<?php
+
+use app\models\Comentario;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+
+/** @var yii\web\View $this */
+/** @var app\models\ComentarioSearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = 'Mis Comentarios';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="comentario-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?php if (!empty($comentarios)): ?>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Comentario</th>
+                    <th>Oferta</th>
+                    <th>Fecha de Creación</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($comentarios as $comentario): ?>
+                    <tr>
+                        <td><?= Html::encode($comentario->texto) ?></td>
+                        <td>
+                            <?= $comentario->oferta
+                                ? Html::a(
+                                    Html::encode($comentario->oferta->titulo),
+                                    ['ofertas/view', 'id' => $comentario->oferta->id]
+                                )
+                                : 'Oferta no encontrada';
+                            ?>
+                        </td>
+                        <td><?= Html::encode($comentario->fecha_creacion) ?></td>
+                        <td>
+                            <?= Html::a('Editar', ['update', 'id' => $comentario->id], ['class' => 'btn btn-primary btn-sm']) ?>
+                            <?= Html::a('Eliminar', ['delete', 'id' => $comentario->id], [
+                                'class' => 'btn btn-danger btn-sm',
+                                'data' => [
+                                    'confirm' => '¿Estás seguro de que quieres eliminar este comentario?',
+                                    'method' => 'post',
+                                ],
+                            ]) ?>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    <?php else: ?>
+        <p>No has realizado ningún comentario todavía.</p>
+    <?php endif; ?>
+</div>
