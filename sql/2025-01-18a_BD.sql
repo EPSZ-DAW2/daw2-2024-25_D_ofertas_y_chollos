74806c951d24a0b85d72af85e2b9d57b3e74bfc0
aaronmc26 <150802257+aaronmc26@users.noreply.github.com>
2025-01-18 20:04:17 +0100

    nueva columna para anuncios

--- sql/2025-01-18a_BD.sql
+++ sql/2025-01-18a_BD.sql
@@ -1,3 +1,5 @@
 ALTER TABLE `ofertas` ADD COLUMN `patrocinador_id` INT NULL AFTER `proveedor_id`;
 
 ALTER TABLE `ofertas` ADD CONSTRAINT `fk_ofertas_patrocinador_id` FOREIGN KEY (`patrocinador_id`) REFERENCES `usuarios`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+ALTER TABLE `anuncios` ADD COLUMN `estado` VARCHAR(20) NOT NULL DEFAULT 'visible' AFTER `oferta_id`;
