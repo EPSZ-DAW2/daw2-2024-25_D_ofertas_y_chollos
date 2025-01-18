8d8228de99efeb7065fd74a278c1b6c3a95c18aa
aaronmc26 <150802257+aaronmc26@users.noreply.github.com>
2025-01-18 18:02:26 +0100

    cambios en vistas de logs

--- codigo/views/logs/index.php
+++ codigo/views/logs/index.php
@@ -18,7 +18,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a(Yii::t('app', 'Create Log'), ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a(Yii::t('app', 'Crear Log'), ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
