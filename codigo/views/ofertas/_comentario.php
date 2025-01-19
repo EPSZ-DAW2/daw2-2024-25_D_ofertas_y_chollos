81fed7106ea8b63b21ad015dda8aac83fa3e649a
Álvaro Arango <git@alvaroarango.com>
2025-01-19 16:40:26 +0100

    Modal para motivo de denuncia

--- codigo/views/ofertas/_comentario.php
+++ codigo/views/ofertas/_comentario.php
@@ -1,6 +1,7 @@
 <?php
 
 use yii\helpers\Html;
+use yii\widgets\ActiveForm;
 
 ?>
 
@@ -11,12 +12,39 @@ use yii\helpers\Html;
         <?= Html::a('Responder', ['comentario/create', 'oferta_id' => $comentario->oferta_id, 'comentario_origen_id' => $comentario->id], ['class' => 'btn btn-link']) ?>
     <?php endif; ?>
     <?php if (!$comentario->cerrado && Yii::$app->user->id !== $comentario->usuario_id): ?>
-        <?= Html::a('Denunciar', ['comentario/denunciar', 'id' => $comentario->id], [
+        <?= Html::a('Denunciar', '#', [
             'class' => 'btn btn-link text-danger',
-            'data' => [
-                'confirm' => '¿Estás seguro de que deseas denunciar este comentario?',
-            ],
+            'data-toggle' => 'modal',
+            'data-target' => "#modalDenuncia{$comentario->id}"
         ]) ?>
+
+        <!-- Modal de Denuncia -->
+        <div class="modal fade" id="modalDenuncia<?= $comentario->id ?>" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Denunciar Comentario</h5>
+                        <button type="button" class="close" data-dismiss="modal">
+                            <span>&times;</span>
+                        </button>
+                    </div>
+                    <?php $form = ActiveForm::begin([
+                        'action' => ['comentario/denunciar', 'id' => $comentario->id]
+                    ]); ?>
+                    <div class="modal-body">
+                        <?= $form->field($comentario, 'motivo_denuncia')->textarea([
+                            'rows' => 4,
+                            'placeholder' => 'Describe el motivo de la denuncia'
+                        ])->label('Motivo de la denuncia') ?>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                        <?= Html::submitButton('Denunciar', ['class' => 'btn btn-danger']) ?>
+                    </div>
+                    <?php ActiveForm::end(); ?>
+                </div>
+            </div>
+        </div>
     <?php endif; ?>
     <?php if (Yii::$app->user->can('admin')): ?>
         <?php if (!$comentario->bloqueado): ?>
