897114e2647bc0999cd09c5345ddc8dba2c1d54d
Álvaro Arango <git@alvaroarango.com>
2025-01-19 16:34:23 +0100

    Update revisar-denuncias.php

--- codigo/views/comentario/revisar-denuncias.php
+++ codigo/views/comentario/revisar-denuncias.php
@@ -3,10 +3,6 @@ use yii\helpers\Html;
 use yii\grid\GridView;
 use yii\widgets\Pjax;
 
-/* @var $this yii\web\View */
-/* @var $searchModel app\models\ComentarioSearch */
-/* @var $dataProvider yii\data\ActiveDataProvider */
-
 $this->title = 'Revisar Denuncias';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
@@ -25,36 +21,68 @@ $this->params['breadcrumbs'][] = $this->title;
 		'columns' => [
 			['class' => 'yii\grid\SerialColumn'],
 
-			'id',
-			'oferta_id',
+			[
+				'attribute' => 'usuario.nombre',
+				'label' => 'Usuario',
+			],
+			[
+				'attribute' => 'oferta.titulo',
+				'label' => 'Oferta',
+				'format' => 'raw',
+				'value' => function($model) {
+					return Html::a(
+						Html::encode($model->oferta->titulo),
+						['/ofertas/view', 'id' => $model->oferta_id]
+					);
+				}
+			],
 			'texto:ntext',
 			'denuncias',
-			'motivo_denuncia:ntext',
-			'fecha_primer_denuncia',
+			'fecha_primer_denuncia:datetime',
+			[
+				'attribute' => 'bloqueado',
+				'value' => function($model) {
+					return $model->bloqueado ? 'Sí' : 'No';
+				},
+				'filter' => [1 => 'Sí', 0 => 'No'],
+			],
 
 			[
 				'class' => 'yii\grid\ActionColumn',
-				'template' => '{view} {bloquear} {eliminar}',
+				'template' => '{view} {bloquear} {desbloquear} {delete}',
 				'buttons' => [
-					'bloquear' => function($url, $model, $key) {
+					'view' => function($url, $model) {
+						return Html::a('<i class="fas fa-eye"></i>', ['view', 'id' => $model->id], [
+							'class' => 'btn btn-info btn-sm',
+							'title' => 'Ver detalle'
+						]);
+					},
+					'bloquear' => function($url, $model) {
 						if (!$model->bloqueado) {
 							return Html::a('Bloquear', ['bloquear', 'id' => $model->id], [
 								'class' => 'btn btn-danger btn-sm',
 								'data' => [
-									'confirm' => '¿Estás seguro de que deseas bloquear este comentario?',
-									'method' => 'post',
-								],
+									'confirm' => '¿Estás seguro de que deseas bloquear este comentario?'
+								]
 							]);
 						}
-						return '';
 					},
-					'eliminar' => function($url, $model, $key) {
+					'desbloquear' => function($url, $model) {
+						if ($model->bloqueado) {
+							return Html::a('Desbloquear', ['desbloquear', 'id' => $model->id], [
+								'class' => 'btn btn-success btn-sm',
+								'data' => [
+									'confirm' => '¿Estás seguro de que deseas desbloquear este comentario?'
+								]
+							]);
+						}
+					},
+					'delete' => function($url, $model) {
 						return Html::a('Eliminar', ['delete', 'id' => $model->id], [
 							'class' => 'btn btn-danger btn-sm',
 							'data' => [
-								'confirm' => '¿Estás seguro de que deseas eliminar este comentario?',
-								'method' => 'post',
-							],
+								'confirm' => '¿Estás seguro de que deseas eliminar este comentario?'
+							]
 						]);
 					},
 				],
@@ -62,5 +90,4 @@ $this->params['breadcrumbs'][] = $this->title;
 		],
 	]); ?>
 	<?php Pjax::end(); ?>
-
 </div>
