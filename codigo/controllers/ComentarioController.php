57c9e8ed4db82e168804b986c9c5c538232bf146
Álvaro Arango <git@alvaroarango.com>
2025-01-19 03:00:04 +0100

    Update ComentarioController.php

--- codigo/controllers/ComentarioController.php
+++ codigo/controllers/ComentarioController.php
@@ -196,7 +196,7 @@ class ComentarioController extends Controller
     protected function findModel($id)
     {
         $model = Comentario::findOne(['id' => $id]));
-        if (($model !== null && $model->usuario_id === Yii::$app->user->id) {
+        if ($model !== null && $model->usuario_id === Yii::$app->user->id) {
             return $model;
         }
 
