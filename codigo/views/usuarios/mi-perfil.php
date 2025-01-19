c032d721d205ffff4c0bf53c4d9f636b2386b1ae
javimanso <jamancon5@gmail.com>
2025-01-19 16:23:35 +0100

    Tablas de Rbac

--- codigo/views/usuarios/mi-perfil.php
+++ codigo/views/usuarios/mi-perfil.php
@@ -6,10 +6,13 @@ use yii\widgets\ActiveForm;
 /** @var yii\web\View $this */
 /** @var app\models\Usuarios $model */
 /** @var yii\widgets\ActiveForm $form */
+
+
+$this->title = 'Mi Perfil';
+$this->params['breadcrumbs'][] = $this->title;
 ?>
-
-<div class="usuarios-form">
-
+<div class="perfil">
+    <h1><?= Html::encode($this->title) ?></h1>
 
 
     <?php if (Yii::$app->session->hasFlash('success')): ?>
@@ -25,85 +28,125 @@ use yii\widgets\ActiveForm;
     <?php endif; ?>
 
 
-    <?php $form = ActiveForm::begin(); ?>
+    <div class="usuarios-form">
+        <h3>Datos de Usuario</h3>
+        <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'nick')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'nombre')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'apellidos')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'nick')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'nombre')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'apellidos')->textInput(['maxlength' => true]) ?>
-
-
-    <div class="form-group">
-        <?= Html::submitButton('Guardar', ['class' => 'btn btn-primary']) ?>
+        <div class="form-group">
+            <?= Html::submitButton('Guardar', ['class' => 'btn btn-primary']) ?>
+        </div>
+        <?php ActiveForm::end(); ?>
     </div>
-    <?php ActiveForm::end(); ?>
 
+
+    <hr>
+
+    <div class="perfil-opciones">
+        <h3>Opciones del Perfil</h3>
+
+        <?= Html::a('Cambiar Contraseña', ['usuarios/cambiar-contrasena'], ['class' => 'btn btn-warning']) ?>
+
+        <?= Html::a(
+            isset($mensajesNuevos) && $mensajesNuevos > 0
+                ? "Notificaciones ({$mensajesNuevos})"
+                : 'Notificaciones',
+            ['mensajes/index'],
+            ['class' => isset($mensajesNuevos) && $mensajesNuevos > 0 ? 'btn btn-primary' : 'btn']
+        ) ?>
+        <?= Html::a(
+            isset($mensajesAdminNuevos) && $mensajesAdminNuevos > 0
+                ? "Avisos de Administrador ({$mensajesAdminNuevos})"
+                : 'Avisos de Administrador',
+            ['mensajes/admin'],
+            ['class' => isset($mensajesAdminNuevos) && $mensajesAdminNuevos > 0 ? 'btn btn-primary' : 'btn btn-secondary']
+        ) ?>
+        <?= Html::a('Alertas de Ofertas', ['alertas-ofertas/index'], ['class' => 'btn btn-primary']) ?>
+
+    </div>
+
+    <hr>
+
+
+
+
+
+
+    <div class="perfil-mensajes">
+        <h3>Mensajes Nuevos</h3>
+        <?php if (isset($mensajesNuevos) && $mensajesNuevos > 0): ?>
+            <ul>
+                <?php
+                $mensajes = \app\models\Mensajes::find()
+                    ->where(['usuario_destino_id' => Yii::$app->user->identity->id])
+                    ->andWhere(['>', 'fecha_hora', Yii::$app->session->get('fechaUltimoAccesoAnterior', '2000-01-01 00:00:00')])
+                    ->all();
+
+                foreach ($mensajes as $mensaje): ?>
+                    <li><?= Html::encode($mensaje->texto) ?> - <em><?= $mensaje->fecha_hora ?></em></li>
+                <?php endforeach; ?>
+            </ul>
+        <?php else: ?>
+            <p>No tienes mensajes nuevos.</p>
+        <?php endif; ?>
+    </div>
+
+    <hr>
+
+
+
+    <div class="perfil-funcionalidades">
+        <h3>Más Opciones</h3>
+        <div class="row">
+            <div class="col-md-4">
+                <?= Html::a('Mis Comentarios', ['usuario-comentario/index'], ['class' => 'btn btn-info btn-block']) ?>
+            </div>
+            <div class="col-md-4">
+                <?= Html::a('Seguimiento de Ofertas', ['seguimiento-ofertas/index'], ['class' => 'btn btn-info btn-block']) ?>
+            </div>
+            <div class="col-md-4">
+                <?= Html::a('Seguimiento de Etiquetas', ['seguimiento-etiquetas/index'], ['class' => 'btn btn-info btn-block']) ?>
+            </div>
+
+            <div class="col-md-4">
+                <?= Html::a('Preferencias', ['preferencias/index'], ['class' => 'btn btn-info btn-block']) ?>
+            </div>
+            <div class="col-md-4">
+                <?= Html::a('Mis Ofertas', ['ofertas-usuarios/index'], ['class' => 'btn btn-info btn-block']) ?>
+            </div>
+            <div class="col-md-4">
+                <?= Html::a('Solicitar Baja', ['usuarios/solicitar-baja'], [
+                    'class' => 'btn btn-info btn-block',
+                    'data' => [
+                        'confirm' => '¿Estás seguro de que quieres solicitar la baja? Esta acción es irreversible.',
+                        'data-method' => 'post',
+                    ],
+                ]) ?>
+            </div>
+        </div>
+    </div>
 </div>
 
 
-<?php
-$fechaUltimoAccesoAnterior = Yii::$app->session->get('fechaUltimoAccesoAnterior', 'No disponible');
-?>
-<hr>
 
-<div class="perfil-opciones">
-    <h3>Opciones del perfil</h3>
+<style>
+    .perfil {
+        max-width: 900px;
+        margin: 0 auto;
+    }
 
+    .perfil-opciones,
+    .perfil-mensajes,
+    .perfil-funcionalidades {
+        margin-top: 20px;
+    }
 
-
-    <?= Html::a('Cambiar Contraseña', ['usuarios/cambiar-contrasena'], ['class' => 'btn btn-warning']) ?>
-
-    <?= Html::a('Solicitar Baja', ['usuarios/solicitar-baja'], [
-        'class' => 'btn btn-danger',
-        'data' => [
-            'confirm' => '¿Estás seguro de que quieres solicitar la baja? Esta acción es irreversible.',
-            'method' => 'post',
-        ],
-    ]) ?>
-
-    <?= Html::a(
-        isset($mensajesNuevos) && $mensajesNuevos > 0
-            ? "Notificaciones ({$mensajesNuevos})"
-            : 'Notificaciones',
-        ['mensajes/index'],
-        ['class' => isset($mensajesNuevos) && $mensajesNuevos > 0 ? 'btn btn-primary' : 'btn']
-    ) ?>
-
-
-    <?= Html::a(
-        isset($mensajesAdminNuevos) && $mensajesAdminNuevos > 0
-            ? "Avisos de Administrador ({$mensajesAdminNuevos})"
-            : 'Avisos de Administrador',
-        ['mensajes/admin'],
-        ['class' => isset($mensajesAdminNuevos) && $mensajesAdminNuevos > 0 ? 'btn btn-primary' : 'btn btn-secondary']
-    ) ?>
-
-
-
-
-
-
-    <?php if ($mensajesNuevos > 0): ?>
-        <h4>Mensajes Nuevos:</h4>
-        <ul>
-            <?php
-            $mensajes = \app\models\Mensajes::find()
-                ->where(['usuario_destino_id' => Yii::$app->user->identity->id])
-                ->andWhere(['>', 'fecha_hora', Yii::$app->session->get('fechaUltimoAccesoAnterior', '2000-01-01 00:00:00')])
-                ->all();
-
-            foreach ($mensajes as $mensaje): ?>
-                <li><?= Html::encode($mensaje->texto) ?> - <em><?= $mensaje->fecha_hora ?></em></li>
-            <?php endforeach; ?>
-        </ul>
-    <?php else: ?>
-        <p>No tienes mensajes nuevos.</p>
-    <?php endif; ?>
-
-
-
-
-</div>
+    .btn-block {
+        width: 100%;
+        margin-bottom: 10px;
+    }
+</style>
---
+++ sql/2025-01-19-TablasAuth.sql
@@ -0,0 +1,182 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 19-01-2025 a las 16:21:32
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `daw_proyectod`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `auth_assignment`
+--
+
+CREATE TABLE `auth_assignment` (
+  `item_name` varchar(64) NOT NULL,
+  `user_id` varchar(64) NOT NULL,
+  `created_at` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `auth_assignment`
+--
+
+INSERT INTO `auth_assignment` (`item_name`, `user_id`, `created_at`) VALUES
+('admin', '2', 1737130748),
+('admin', '3', 1737130748),
+('admin', '70', 1737158885),
+('moderador', '5', 1737130748),
+('moderador', '6', 1737130748),
+('moderador', '72', 1737159453),
+('normal', '45', 1737130748),
+('normal', '46', 1737130748),
+('normal', '47', 1737130748),
+('normal', '71', 1737159302),
+('patrocinador', '33', 1737130748),
+('patrocinador', '34', 1737130748),
+('patrocinador', '73', 1737159436);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `auth_item`
+--
+
+CREATE TABLE `auth_item` (
+  `name` varchar(64) NOT NULL,
+  `type` int(11) NOT NULL,
+  `description` text DEFAULT NULL,
+  `rule_name` varchar(64) DEFAULT NULL,
+  `data` longblob DEFAULT NULL,
+  `created_at` int(11) DEFAULT NULL,
+  `updated_at` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `auth_item`
+--
+
+INSERT INTO `auth_item` (`name`, `type`, `description`, `rule_name`, `data`, `created_at`, `updated_at`) VALUES
+('accederFichaUsuariosAdmin', 2, 'Permiso para acceder a la ficha de usuarios del administrador', NULL, NULL, 1737133083, 1737133083),
+('admin', 1, 'Administrador del sistema', NULL, NULL, 1737130748, 1737130748),
+('moderador', 1, 'Moderador de contenido', NULL, NULL, 1737130748, 1737130748),
+('normal', 1, 'Usuario normal del sistema', NULL, NULL, 1737130748, 1737130748),
+('patrocinador', 1, 'Patrocinador de ofertas', NULL, NULL, 1737130748, 1737130748),
+('permisosAdministrador', 2, 'Permisos avanzados para administradores', NULL, NULL, NULL, NULL),
+('permisosBasicos', 2, 'Permisos para usuarios normales', NULL, NULL, NULL, NULL),
+('permisosModerador', 2, 'Permisos para moderadores', NULL, NULL, NULL, NULL),
+('permisosProveedor', 2, 'Permisos para proveedores', NULL, NULL, NULL, NULL),
+('sysadmin', 1, 'Super Administrador del sistema', NULL, NULL, 1737130748, 1737130748);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `auth_item_child`
+--
+
+CREATE TABLE `auth_item_child` (
+  `parent` varchar(64) NOT NULL,
+  `child` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `auth_item_child`
+--
+
+INSERT INTO `auth_item_child` (`parent`, `child`) VALUES
+('admin', 'accederFichaUsuariosAdmin'),
+('admin', 'permisosAdministrador'),
+('permisosAdministrador', 'permisosBasicos'),
+('permisosAdministrador', 'permisosModerador'),
+('permisosAdministrador', 'permisosProveedor'),
+('permisosModerador', 'permisosBasicos'),
+('permisosProveedor', 'permisosBasicos'),
+('sysadmin', 'accederFichaUsuariosAdmin'),
+('sysadmin', 'permisosAdministrador');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `auth_rule`
+--
+
+CREATE TABLE `auth_rule` (
+  `name` varchar(64) NOT NULL,
+  `data` longblob DEFAULT NULL,
+  `created_at` int(11) DEFAULT NULL,
+  `updated_at` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `auth_assignment`
+--
+ALTER TABLE `auth_assignment`
+  ADD PRIMARY KEY (`item_name`,`user_id`);
+
+--
+-- Indices de la tabla `auth_item`
+--
+ALTER TABLE `auth_item`
+  ADD PRIMARY KEY (`name`),
+  ADD KEY `rule_name` (`rule_name`);
+
+--
+-- Indices de la tabla `auth_item_child`
+--
+ALTER TABLE `auth_item_child`
+  ADD PRIMARY KEY (`parent`,`child`),
+  ADD KEY `child` (`child`);
+
+--
+-- Indices de la tabla `auth_rule`
+--
+ALTER TABLE `auth_rule`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `auth_assignment`
+--
+ALTER TABLE `auth_assignment`
+  ADD CONSTRAINT `auth_assignment_ibfk_1` FOREIGN KEY (`item_name`) REFERENCES `auth_item` (`name`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `auth_item`
+--
+ALTER TABLE `auth_item`
+  ADD CONSTRAINT `auth_item_ibfk_1` FOREIGN KEY (`rule_name`) REFERENCES `auth_rule` (`name`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `auth_item_child`
+--
+ALTER TABLE `auth_item_child`
+  ADD CONSTRAINT `auth_item_child_ibfk_1` FOREIGN KEY (`parent`) REFERENCES `auth_item` (`name`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `auth_item_child_ibfk_2` FOREIGN KEY (`child`) REFERENCES `auth_item` (`name`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
