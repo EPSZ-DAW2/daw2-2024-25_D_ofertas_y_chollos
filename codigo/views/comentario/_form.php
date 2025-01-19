487e68a3e94a76cb5c10775e93d392a501e3e76a
Álvaro Arango <git@alvaroarango.com>
2025-01-19 02:35:17 +0100

    Comentarios: mostrar solo campos editables

--- codigo/views/comentario/_form.php
+++ codigo/views/comentario/_form.php
@@ -12,34 +12,14 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'oferta_id')->textInput() ?>
-
     <?= $form->field($model, 'texto')->textarea(['rows' => 6]) ?>
 
-    <?= $form->field($model, 'comentario_origen_id')->textInput() ?>
-
-    <?= $form->field($model, 'cerrado')->textInput() ?>
-
-    <?= $form->field($model, 'denuncias')->textInput() ?>
-
-    <?= $form->field($model, 'fecha_primer_denuncia')->textInput() ?>
-
-    <?= $form->field($model, 'motivo_denuncia')->textarea(['rows' => 6]) ?>
-
-    <?= $form->field($model, 'bloqueado')->textInput() ?>
-
-    <?= $form->field($model, 'fecha_bloqueo')->textInput() ?>
-
-    <?= $form->field($model, 'motivo_bloqueo')->textarea(['rows' => 6]) ?>
-
-    <?= $form->field($model, 'usuario_id')->textInput() ?>
-
-    <?= $form->field($model, 'fecha_creacion')->textInput() ?>
-
-    <?= $form->field($model, 'fecha_modificacion')->textInput() ?>
+    <?php if (!$model->cerrado): ?>
+        <?= $form->field($model, 'comentario_origen_id')->hiddenInput()->label(false) ?>
+    <?php endif; ?>
 
     <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+        <?= Html::submitButton('Guardar Comentario', ['class' => 'btn btn-success']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
